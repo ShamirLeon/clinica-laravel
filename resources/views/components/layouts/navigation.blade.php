93a64f904a2666b0932948b8b503e3065b5f4fcf
Shamir León <leonshamir1@gmail.com>
2024-01-07 18:48:18 -0500

    add navigation component

---
+++ resources/views/components/layouts/navigation.blade.php
@@ -0,0 +1,5 @@
+<ul>
+    <li><a href="{{ route('home') }}">Home</a></li>
+    <li><a href="{{ route('doctors') }}">Doctors</a></li>
+    <li><a href="{{ route('patients') }}">Patients</a></li>
+</ul>
